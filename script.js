dc7bfe45a116ddc3786d1a29de6d23e44e9046ba
Ivan G <132954889+ivang-dev@users.noreply.github.com>
2024-11-21 13:08:03 -0800

    Added the creation of rows, trying to get the blocks to appear as well

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ divBlock.classList.add("divBlock");
 // Create the selector for the main div grid
 const divGrid = document.querySelector("#divGrid");
 
-// Event listener for the full page to laod
+// Create another div to become the rows
+const divRow = document.createElement("div");
+
+// Add class to row divs
+divRow.classList.add("divRow");
+
+// Event listener for the full page to load
 window.addEventListener("load", function () {
     console.log("it load")
-
-    // Create and append the 16 blocks to main div container
+    // Loop to create 16 rows with 16 divs inside
     for (let i = 0; i < 16; i++) {
-        divGrid.append(divBlock.cloneNode());
+        divGrid.append(divRow.cloneNode());
+        for (let j = 0; j < 16; j++) {
+            console.log("?")
+            divRow.appendChild(divBlock.cloneNode());
+        }
     }
 });
