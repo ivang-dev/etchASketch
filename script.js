cffd2e1fafbef49ab1fd9193afb3854fd76df177
Ivan G <132954889+ivang-dev@users.noreply.github.com>
2024-11-21 13:36:55 -0800

    Reworked the loop code for the rows and columns, now loops perfectly

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-// Create basic div element before multiplying by 16
-const divBlock = document.createElement("div");
-
-// Add class to these divs
-divBlock.classList.add("divBlock");
+// Constants for the rows and columns
+const numOfRows = 16;
+const numOfCols = 16;
 
 // Create the selector for the main div grid
 const divGrid = document.querySelector("#divGrid");
 
-// Create another div to become the rows
-const divRow = document.createElement("div");
-
-// Add class to row divs
-divRow.classList.add("divRow");
-
 // Event listener for the full page to load
 window.addEventListener("load", function () {
-    console.log("it load")
-    // Loop to create 16 rows with 16 divs inside
-    for (let i = 0; i < 16; i++) {
-        divGrid.append(divRow.cloneNode());
-        for (let j = 0; j < 16; j++) {
-            console.log("?")
-            divRow.appendChild(divBlock.cloneNode());
+    // Outer for loop for the row divs
+    for (let i = 0; i <numOfRows; i++) {
+        // Create div to become the rows
+        const divRow = document.createElement("div");
+
+        // Add class to row divs
+        divRow.className = "divRow";
+
+        // Inner loop for the columns/cells in each row
+        for (let j = 0; j < numOfCols; j++) {
+            // Create basic div element before multiplying by 16
+            const divBlock = document.createElement("div");
+
+            // Add class to these divs
+            divBlock.className = "divBlock";
+
+            // Append to the row divs
+            divRow.append(divBlock);
         }
+        // Append the row to the main grid
+        divGrid.append(divRow);
     }
+
 });
